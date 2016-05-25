440b14a14f617a93c23aba939e92a9cc039a4b4d
Daniel Stewart <daniel.robbins.stewart@gmail.com>
2016-05-25 12:25:33 -0600

    Setup routes and styled Dashboard

--- src/components/IconLink.js
+++ src/components/IconLink.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router'
 const IconLink = (props) => (
   <Link className="col-xs-3 icon-link" style={{backgroundColor: props.color}} to={props.pathTo}>
     <h2>{props.title}</h2>
+    <span className={props.icon}></span>
   </Link>
 );
 export default IconLink
--- src/components/SpeakBox.js
+++ src/components/SpeakBox.js
@@ -19,7 +19,7 @@ class SpeakBox extends Component {
   }
   render(){
     return (
-      <div>
+      <div className="text-center">
         {voices.map((x, i) =>{
           console.log(x);
         })}
