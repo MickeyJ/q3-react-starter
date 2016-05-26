34a3077ad01651239a955bf0a970fd0bb2af5577
Anthony Simpson <ansi6622@colorado.edu>
2016-05-26 15:36:33 -0600

    form stuff

--- src/pages/dashboard/MyPhrases.js
+++ src/pages/dashboard/MyPhrases.js
@@ -11,10 +11,61 @@ class MyPhrasesPage extends React.Component{
     setVoice(msg, 'Bad News');
     sayMessage(msg, text);
   }
+
+  getUserId(ref){
+    this.id = ref
+  }
+  getPhraseText(ref){
+    this.phrase = ref
+  }
+  onInputChange(e){
+    // console.log(this.email.value);
+  }
+  handleSubmit(e){
+    e.preventDefault();
+    let user_id = this.id.value,
+        phrase = this.phrase.value
+    if(!user_id || !phrase) {
+      this.setState({
+        error: 'yo shit fucked'
+      });
+      return false;
+    }
+    return (
+        this.props.phrasePush({user_id, phrase})
+            .then(res =>{
+              if(res.payload.data){
+                console.log(res.payload.data)
+              } else {
+                this.setState({
+                  error: res.payload.data.error
+                });
+              }
+            })
+    )
+  }
   render(){
     return(
       <div>
         <h1 className="text-center">My Phrases</h1>
+        <form className="col-md-6 col-md-offset-3 auth-form" onSubmit={this.handleSubmit.bind(this)}>
+          <h2>Add Up</h2>
+          <ErrorBox error={this.state.error}/>
+          <input
+              type="hidden"
+              className="form-control register"
+              value={JWT.fetch()}
+              ref={(ref) => this.getUserId(ref)}
+          />
+          <input
+              type="text"
+              placeholder="phrase it"
+              className="form-control register"
+              ref={(ref) => this.getPhraseText(ref)}
+              onChange={(e) => this.onInputChange(e)}
+          />
+          <button className="btn btn-success" type="submit">Sign Up</button>
+        </form>
       </div>
     )
   }
