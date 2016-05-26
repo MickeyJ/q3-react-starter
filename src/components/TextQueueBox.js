086c4cb6e2b208374b8c8471eafd59d2badc423a
Mike Homolak <mhomolak@yahoo.com>
2016-05-26 16:27:22 -0700

    fixed types in files / file names

---
+++ src/components/TextQueueBox.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const TextQueueBox = props => (
+  <section id="text-Queue-box">
+
+    <article id="text-Queue-input">
+      {props.textQueue.map((x, i) =>(
+        <span
+          key={i}
+          onClick={() => props.removeFromQueue(i)}
+          className="btn btn-primary word-to-Queue" >
+                {x}
+              </span>
+      ))}
+    </article>
+
+    <button
+      id="say-Queue-btn"
+      className="btn btn-success"
+      onClick={props.sayQueue.bind(this)}>
+      Say It
+    </button>
+    <button
+      id="clear-Queue-btn"
+      className="btn btn-warning"
+      onClick={() => props.setQueueState( [] )}>
+      Clear Queue
+    </button>
+
+  </section>
+);
+
+export default TextQueueBox
