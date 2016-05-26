0a7357a2c6718fb313c378a061ad4e66b83f31be
Mickey <develop.agility@gmail.com>
2016-05-26 13:13:33 -0600

    improved speech construction funcionality

--- src/pages/dashboard/Constructor.js
+++ src/pages/dashboard/Constructor.js
@@ -15,27 +15,36 @@ class ConstructorPage extends Component{
   getInputText(ref){
     this.inputText = ref
   }
+  setCueState(input){
+    this.setState({ textCue: input });
+  }
+  pushToCue(input){
+    this.cue.push(input + ' ');
+    this.setCueState(this.cue);
+  }
   addToCue(e, input){
     e.preventDefault();
     this.cue = this.state.textCue;
-    this.cue.push(input + ' ');
-    this.setState({
-      textCue: this.cue
-    });
+    if(e.type === "submit"){
+      if(!this.inputText.value) return false;
+      else {
+        this.pushToCue(input);
+        this.inputText.value = null
+      }
+    } 
+    else 
+      this.pushToCue(input);
   }
   removeFromCue(index){
-    this.cue = this.state.textCue.filter((x, i) =>(
-      i !== index
-    ));
-    this.setState({
-      textCue: this.cue
-    })
+    this.cue = this.state.textCue;
+    this.cue = this.cue.filter((x, i) =>i !== index);
+    this.setCueState(this.cue);
   }
-  sayIt(){
+  sayCue(){
     const msg = new SpeechSynthesisUtterance();
     this.textToSpeak = '';
-    this.state.textCue.map(x =>{
-      this.textToSpeak += x + ' '
+    this.state.textCue.map(text =>{
+      this.textToSpeak += text + ' '
     });
     setVoice(msg, 'Samantha');
     sayMessage(msg, this.textToSpeak);
@@ -45,6 +54,7 @@ class ConstructorPage extends Component{
       <div>
         <h1 className="text-center">Speech Constructor</h1>
         <section id="text-cue-box">
+
           <article id="text-cue-input">
             {this.state.textCue.map((x, i) =>(
               <span
@@ -55,14 +65,25 @@ class ConstructorPage extends Component{
               </span>
             ))}
           </article>
-          <button onClick={this.sayIt.bind(this)}>
+
+          <button
+            id="say-cue-btn"
+            className="btn btn-success"
+            onClick={this.sayCue.bind(this)}>
             Say It
           </button>
+          <button
+            id="clear-cue-btn"
+            className="btn btn-warning"
+            onClick={() => this.setCueState([])}>
+            Clear Cue
+          </button>
+
         </section>
 
         <section id="word-box">
           <div className='col-xs-6'>
-            <h3 className='text-center'>Words</h3>
+            <h3 className='text-center'>Words / Phrases</h3>
             {phrases.map((x, i) =>(
               <span
                 key={i}
@@ -76,18 +97,18 @@ class ConstructorPage extends Component{
 
         <div className='col-xs-6'>
           <h3 className='text-center'>Categories?</h3>
-          <form className="form-inline" onSubmit={(e) =>this.addToCue(e, this.inputText.value)}>
+          <form className="form-inline add-text-form" onSubmit={(e) =>this.addToCue(e, this.inputText.value)}>
             <input
               type="text"
               className="form-control speak-input"
-              placeholder="Add To Cue"
+              placeholder="Type Here"
               ref={(ref) => this.getInputText(ref)}
             />
             <button
               type="submit"
-              className="btn btn-info"
-              onClick={(e) =>this.addToCue(e, this.inputText.value)}
-            >Add To Cue</button>
+              className="btn btn-info">
+              Add To Cue
+            </button>
           </form>
         </div>
 
