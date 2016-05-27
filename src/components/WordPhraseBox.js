01d5062aa9172491f8ec18f4deece1f1b9f6fef8
Daniel Stewart <daniel.robbins.stewart@gmail.com>
2016-05-26 18:09:57 -0600

    Built out word bank for Constructor page

--- src/components/WordPhraseBox.js
+++ src/components/WordPhraseBox.js
@@ -1,16 +1,55 @@
 import React from 'react'
 
-import { phrases, words }from '../data/default'
+import { phrases, words, pronouns, verbs, negatives, adjectives, preps }from '../data/default'
 
 const WordPhraseBox = props => (
   <section id="word-box">
-    <div className='col-xs-6'>
-      <h3 className='text-center'>Words / Phrases</h3>
-      {phrases.map((x, i) =>(
+    <div className='col-xs-2 word-bank-1'>
+      {pronouns.map((x, i) =>(
         <span
           key={i}
           onClick={(e) => props.addToCue(e, x)}
-          className='btn btn-primary word-to-cue'>
+          className='btn btn-primary pronouns'>
+              {x}
+            </span>
+      ))}
+    </div>
+    <div className='col-xs-1 word-bank-2'>
+    {negatives.map((x, i) =>(
+      <span
+      key={i}
+      onClick={(e) => props.addToCue(e, x)}
+      className='btn btn-primary negatives'>
+      {x}
+      </span>
+    ))}
+    </div>
+    <div className='col-xs-3 word-bank-3'>
+      {verbs.map((x, i) =>(
+        <span
+          key={i}
+          onClick={(e) => props.addToCue(e, x)}
+          className='btn btn-primary verbs'>
+              {x}
+            </span>
+      ))}
+    </div>
+    <div className='col-xs-2 word-bank-4'>
+      {preps.map((x, i) =>(
+        <span
+        key={i}
+        onClick={(e) => props.addToCue(e, x)}
+        className='btn btn-primary preps'>
+        {x}
+        </span>
+      ))}
+    </div>
+    <div className='col-xs-2 word-bank-5'>
+      {adjectives.map((x, i) =>(
+        <span
+          key={i}
+          onClick={(e) => props.addToCue(e, x)}
+          className='btn btn-primary adjectives'>
               {x}
             </span>
       ))}
